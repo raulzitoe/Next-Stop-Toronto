12713b957fe72f867dd0378be229c37040594bfd
Raul Vieira <raulguil@gmail.com>
2022-11-08 17:13:08 -0500

    Remove unused imports

--- app/src/main/java/com/raulvieira/nextstoptoronto/MainActivity.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/MainActivity.kt
@@ -3,12 +3,7 @@ package com.raulvieira.nextstoptoronto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.raulvieira.nextstoptoronto.navigation.MyAppNavHost
 import com.raulvieira.nextstoptoronto.navigation.Screen
