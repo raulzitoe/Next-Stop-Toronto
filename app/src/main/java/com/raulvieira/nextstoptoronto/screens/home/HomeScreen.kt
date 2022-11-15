cb0e9f190b0abc7562081bd564779a78125fa380
Raul Vieira <raulguil@gmail.com>
2022-11-15 16:51:49 -0500

    Grid Layout for routes

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/home/HomeScreen.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/home/HomeScreen.kt
@@ -2,38 +2,66 @@ package com.raulvieira.nextstoptoronto.screens.home
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.raulvieira.nextstoptoronto.navigation.Screen
+import com.raulvieira.nextstoptoronto.RouteModel
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
-    onNavigate: (Screen) -> Unit
+    onNavigate: (String) -> Unit
+
 ) {
     val routes by viewModel.uiState.collectAsState()
-    
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-            LazyColumn(Modifier.fillMaxSize()){
-                items(routes.routeList) { item ->
-                    Row() {
-                        Text(text = item.tag, color = Color.Black)
-                        Spacer(modifier = Modifier.size(5.dp))
-                        Text(text = item.title)
-                    }
-                }
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 150.dp)
+        ) {
+            items(routes.routeList) { route ->
+                RouteCard(modifier = Modifier.height(60.dp), route = route, onClick = { onNavigate(route.tag) })
             }
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RouteCard(modifier: Modifier, route: RouteModel, onClick: () -> Unit) {
+    Card(modifier = modifier.padding(5.dp), onClick = onClick) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = route.title,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(5.dp),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BusCardPreview() {
+    RouteCard(modifier = Modifier, RouteModel("41", "41 - Keele"), onClick = {})
 }
