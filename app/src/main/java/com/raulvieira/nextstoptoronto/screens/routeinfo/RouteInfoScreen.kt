a4ec17deedc1554d7256a3a6b3100be6034464f6
Raul Vieira <raulguil@gmail.com>
2022-11-15 19:08:36 -0500

    Fetch and show route info on screen

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/routeinfo/RouteInfoScreen.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/routeinfo/RouteInfoScreen.kt
@@ -2,18 +2,44 @@ package com.raulvieira.nextstoptoronto.screens.routeinfo
 
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.raulvieira.nextstoptoronto.RouteModel
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import com.raulvieira.nextstoptoronto.models.StopsModel
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
-fun RouteInfoScreen(routeTag: String = "N/A") {
+fun RouteInfoScreen(
+    viewModel: RouteInfoViewModel = hiltViewModel(),
+    routeTag: String = "N/A"
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+
+    LaunchedEffect(key1 = Unit) {
+        lifecycle.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+            launch {
+                viewModel.getRouteConfig(routeTag)
+            }
+        }
+    }
+
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -36,15 +62,36 @@ fun RouteInfoScreen(routeTag: String = "N/A") {
             }
         },
         content = { innerPadding ->
-            Box(modifier = Modifier.consumedWindowInsets(innerPadding)) {
-                Text(text = "route screen")
+            Surface(modifier = Modifier.padding(innerPadding)) {
+                Column {
+                    Text(uiState.route.title)
+                    LazyColumn() {
+                        items(uiState.route.stop) { routeInfo ->
+                            RouteInfoCard(routeInfo = routeInfo)
+                        }
+                    }
+                }
             }
         }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun RouteScreenPreview() {
-    RouteInfoScreen("41")
+fun RouteInfoCard(routeInfo: StopsModel) {
+    Card(
+        modifier = Modifier
+            .height(60.dp)
+            .padding(10.dp)
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = routeInfo.title, textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(5.dp),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/routeinfo/RouteInfoViewModel.kt
@@ -0,0 +1,38 @@
+package com.raulvieira.nextstoptoronto.screens.routeinfo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.raulvieira.nextstoptoronto.Repository
+import com.raulvieira.nextstoptoronto.models.Route2Model
+import com.raulvieira.nextstoptoronto.models.RouteConfigModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RouteInfoViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<RouteConfigModel> = MutableStateFlow(
+        RouteConfigModel(
+            Route2Model(
+                "",
+                arrayListOf()
+            )
+        )
+    )
+    val uiState: StateFlow<RouteConfigModel> = _uiState
+
+    fun getRouteConfig(routeTag: String) {
+        if (routeTag.isNotEmpty()) {
+            viewModelScope.launch {
+                repository.getRouteConfig(routeTag).collect { data ->
+                    if (data == null) return@collect
+                    _uiState.update { data }
+                }
+            }
+        }
+    }
+}
