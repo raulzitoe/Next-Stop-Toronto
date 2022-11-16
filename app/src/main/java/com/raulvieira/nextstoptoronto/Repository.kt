f7164c4e7d076818a347775b6eb135215631d76b
Raul Vieira <raulguil@gmail.com>
2022-11-15 19:06:26 -0500

    Create api functions for routeConfig command

--- app/src/main/java/com/raulvieira/nextstoptoronto/Repository.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/Repository.kt
@@ -6,5 +6,6 @@ class Repository(private val apiService: RetrofitInterface) {
 
     suspend fun getRouteList() = apiService.requestRouteList()
     fun getStopPrediction(stopId: String) = flow { emit(apiService.requestStopPrediction(stopId = stopId).body()) }
+    fun getRouteConfig(routeTag: String) = flow { emit(apiService.requestRouteConfig(routeTag = routeTag).body()) }
 
 }
--- app/src/main/java/com/raulvieira/nextstoptoronto/RetrofitInterface.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/RetrofitInterface.kt
@@ -1,5 +1,8 @@
 package com.raulvieira.nextstoptoronto
 
+import com.raulvieira.nextstoptoronto.models.RouteConfigModel
+import com.raulvieira.nextstoptoronto.models.RouteListModel
+import com.raulvieira.nextstoptoronto.models.StopPredictionModel
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -16,5 +19,11 @@ interface RetrofitInterface {
         @Query("a") agency: String = "ttc",
         @Query("stopId") stopId: String
     ): Response<StopPredictionModel>
+
+    @GET("publicJSONFeed?command=routeConfig")
+    suspend fun requestRouteConfig(
+        @Query("a") agency: String = "ttc",
+        @Query("r") routeTag: String
+    ): Response<RouteConfigModel>
 
 }
