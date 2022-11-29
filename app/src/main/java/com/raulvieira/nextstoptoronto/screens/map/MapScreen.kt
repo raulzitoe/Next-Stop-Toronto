8024e228edcd207c872e0f3547280d4fcd235935
Raul Vieira <raulguil@gmail.com>
2022-11-28 20:17:56 -0500

    Request permissions for map screen

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/map/MapScreen.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/map/MapScreen.kt
@@ -1,24 +1,56 @@
 package com.raulvieira.nextstoptoronto.screens.map
 
+import android.Manifest
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun SecondScreen(
     viewModel: MapScreenViewModel = hiltViewModel()
 ) {
+    val permissionsState =
+        rememberMultiplePermissionsState(
+            permissions = listOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                permissionsState.permissions.first().launchPermissionRequest()
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
+
         Column {
             MapView(
+                modifier = Modifier.fillMaxHeight(0.8f),
                 onRequestStopInfo = { stopId -> viewModel.getStopPrediction(stopId) },
                 stopState = viewModel.stopState,
                 routes = viewModel.getStops()
