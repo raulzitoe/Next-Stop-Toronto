63d0c652ac03b54fbebd5615ba514e83d13c4699
Raul Vieira <raulguil@gmail.com>
2022-12-16 18:52:00 -0500

    Add search to HomeScreen.kt

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/home/HomeScreen.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/home/HomeScreen.kt
@@ -1,15 +1,21 @@
 package com.raulvieira.nextstoptoronto.screens.home
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,12 +34,28 @@ fun HomeScreen(
 
 ) {
     val routes by viewModel.uiState.collectAsStateWithLifecycle()
+    var searchText by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(""))
+    }
+    var searchVisible by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(searchVisible) {
+        if (searchVisible) {
+            focusRequester.requestFocus()
+        }
+    }
 
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Text(stringResource(id = R.string.app_name))
+                },
+                actions = {
+                    IconButton(onClick = { searchVisible = !searchVisible }) {
+                        Icon(Icons.Filled.Search, contentDescription = "Localized description")
+                    }
                 }
             )
         },
@@ -44,16 +66,40 @@ fun HomeScreen(
                     .fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(minSize = 150.dp)
-                ) {
-                    items(items = routes.routeList,
-                        key = { it.routeTag }
-                    ) { route ->
-                        RouteCard(
-                            modifier = Modifier.height(60.dp),
-                            route = route,
-                            onClick = { onNavigate(route.routeTag) })
+                Column {
+                    AnimatedVisibility(visible = searchVisible) {
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 10.dp)
+                                .focusRequester(focusRequester),
+                            value = searchText,
+                            onValueChange = { searchText = it },
+                            label = { Text("Search") }
+                        )
+                    }
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(minSize = 150.dp)
+                    ) {
+                        items(items = routes.routeList.filter {
+                            val words = searchText.text.split("\\s+".toRegex()).map { word ->
+                                word.replace("""^[,\.]|[,\.]$""".toRegex(), "")
+                            }
+                            var containsWord: Boolean = true
+                            words.forEach { word ->
+                                containsWord =
+                                    containsWord && it.title.contains(word, ignoreCase = true)
+                            }
+                            containsWord
+
+                        },
+                            key = { it.routeTag }
+                        ) { route ->
+                            RouteCard(
+                                modifier = Modifier.height(60.dp),
+                                route = route,
+                                onClick = { onNavigate(route.routeTag) })
+                        }
                     }
                 }
             }
