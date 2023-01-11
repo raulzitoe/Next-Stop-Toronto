4e49a933c18130e0e4309643f80db1433f0f1198
Raul Vieira <raulguil@gmail.com>
2023-01-10 19:59:15 -0500

    Improve RouteInfoScreen.kt layout, add Compose Preview

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/routeinfo/RouteInfoScreen.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/routeinfo/RouteInfoScreen.kt
@@ -1,7 +1,6 @@
 package com.raulvieira.nextstoptoronto.screens.routeinfo
 
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,12 +18,14 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.repeatOnLifecycle
+import com.raulvieira.nextstoptoronto.components.AnimatedSearchField
 import com.raulvieira.nextstoptoronto.models.StopModel
 import kotlinx.coroutines.launch
 
@@ -39,14 +40,14 @@ fun RouteInfoScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val lifecycle = LocalLifecycleOwner.current.lifecycle
-    var searchText by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+    var searchedText by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue(""))
     }
     var searchVisible by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
 
     LaunchedEffect(searchVisible) {
-        if (searchVisible){
+        if (searchVisible) {
             focusRequester.requestFocus()
         }
     }
@@ -81,53 +82,71 @@ fun RouteInfoScreen(
         content = { innerPadding ->
             Surface(modifier = Modifier.padding(innerPadding)) {
                 Column {
-                    AnimatedVisibility(visible = searchVisible) {
-                        TextField(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 10.dp).focusRequester(focusRequester),
-                            value = searchText,
-                            onValueChange = { searchText = it },
-                            label = { Text("Search") }
-                        )
-                    }
-                    LazyColumn {
-                        items(
-                            items = uiState.route.stopsList
-                                .filter {
-                                    it.stopId.isNotBlank()
-                                }
-                                .filter {
-                                val words = searchText.text.split("\\s+".toRegex()).map { word ->
-                                    word.replace("""^[,.]|[,.]$""".toRegex(), "")
-                                }
-                                var containsWord = true
-                                words.forEach { word->
-                                   containsWord = containsWord && it.title.contains(word, ignoreCase = true)
-                                }
-                                containsWord
-
-                            }.sortedBy { it.title }, key = { it.stopId }) { routeInfo ->
-                            RouteInfoCard(
-                                routeInfo = routeInfo,
-                                onClick = { stopId -> onClickStop(routeTag, stopId) })
-                        }
-                    }
+                    AnimatedSearchField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp)
+                            .focusRequester(focusRequester),
+                        searchVisible = searchVisible,
+                        searchedText = searchedText,
+                        onValueChange = { searchedText = it })
+                    StopsLazyColumn(
+                        modifier = Modifier.padding(horizontal = 5.dp),
+                        stops = uiState.route.stopsList,
+                        searchedText = searchedText,
+                        onClickStopItem = { stopId -> onClickStop(routeTag, stopId) })
                 }
             }
         }
     )
 }
 
+@Composable
+fun StopsLazyColumn(
+    modifier: Modifier = Modifier,
+    stops: List<StopModel>,
+    searchedText: TextFieldValue,
+    onClickStopItem: (String) -> Unit
+) {
+    LazyColumn(modifier = modifier, verticalArrangement = Arrangement.spacedBy(5.dp)) {
+        items(
+            items = stops
+                .filter {
+                    it.stopId.isNotBlank()
+                }
+                .filter {
+                    val words =
+                        searchedText.text.split("\\s+".toRegex()).map { word ->
+                            word.replace("""^[,.]|[,.]$""".toRegex(), "")
+                        }
+                    var containsWord = true
+                    words.forEach { word ->
+                        containsWord = containsWord && it.title.contains(
+                            word,
+                            ignoreCase = true
+                        )
+                    }
+                    containsWord
+
+                }.sortedBy { it.title }, key = { it.stopId }) { routeInfo ->
+            StopInfoCard(
+                routeInfo = routeInfo,
+                onClick = { stopId -> onClickStopItem(stopId) })
+        }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RouteInfoCard(routeInfo: StopModel, onClick: (stopId: String) -> Unit) {
+fun StopInfoCard(routeInfo: StopModel, onClick: (stopId: String) -> Unit) {
     Card(
         modifier = Modifier
-            .height(60.dp)
-            .padding(10.dp), onClick = { onClick(routeInfo.stopId) }
+            .wrapContentSize(), onClick = { onClick(routeInfo.stopId) }
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .wrapContentHeight().fillMaxWidth().defaultMinSize(minHeight = 48.dp)
+        ) {
             Text(
                 text = routeInfo.title, textAlign = TextAlign.Center,
                 modifier = Modifier
@@ -138,4 +157,34 @@ fun RouteInfoCard(routeInfo: StopModel, onClick: (stopId: String) -> Unit) {
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun AnimatedSearchFieldPreview() {
+    AnimatedSearchField(
+        searchVisible = true,
+        searchedText = TextFieldValue("Text to Search"),
+        onValueChange = {})
+}
+
+@Preview
+@Composable
+fun StopInfoCardPreview() {
+    StopInfoCard(
+        routeInfo = StopModel("1234", "123", "12.4", "12.4", "Dufferin at Somewhere St "),
+        onClick = {})
+}
+
+@Preview
+@Composable
+fun StopsLazyColumnPreview() {
+    StopsLazyColumn(
+        stops = listOf(
+            StopModel(stopId = "1", title = "Stop 1 at that St"),
+            StopModel(stopId = "2", title = "Stop 2 at that St"),
+            StopModel(stopId = "3", title = "Stop 3 at that St")
+        ),
+        searchedText = TextFieldValue(""),
+        onClickStopItem = {})
 }
