58664cce9b50f882dc4d35948ea56f4570a1dd04
Raul Vieira <raulguil@gmail.com>
2023-01-22 18:45:26 -0500

    Create functions to check internet connectivity

---
+++ app/src/main/java/com/raulvieira/nextstoptoronto/utils/NetworkUtils.kt
@@ -0,0 +1,41 @@
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.stateIn
+
+fun isInternetAvailable(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkCapabilities = connectivityManager.activeNetwork ?: return false
+    val actNw =
+        connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+    return when {
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+        else -> false
+    }
+}
+
+fun isInternetOn(context: Context, scope:CoroutineScope) = callbackFlow {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            trySend(true)
+        }
+
+        override fun onLost(network: Network) {
+            trySend(false)
+        }
+    }
+    connectivityManager.registerDefaultNetworkCallback(networkCallback)
+
+    awaitClose {
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+}.stateIn(scope = scope, started = SharingStarted.Eagerly, initialValue = isInternetAvailable(context = context))
