a0f80d338cf164f34a0573ecade9b0f215d258dd
Raul Vieira <raulguil@gmail.com>
2023-02-10 17:11:46 -0500

    Add center to user button on MapScreen

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/map/MapView.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/map/MapView.kt
@@ -1,20 +1,33 @@
 package com.raulvieira.nextstoptoronto.screens.map
 
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.location.Location
 import android.view.MotionEvent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.location.LocationServices
 import com.raulvieira.nextstoptoronto.R
 import com.raulvieira.nextstoptoronto.models.StopModel
 import com.raulvieira.nextstoptoronto.models.StopPredictionModel
+import com.raulvieira.nextstoptoronto.utils.locationFlow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -39,19 +52,48 @@ const val START_ZOOM = 18.0
 const val STARTING_LATITUDE = 43.656339
 const val STARTING_LONGITUDE = -79.460403
 
-@SuppressLint("ClickableViewAccessibility")
+@OptIn(ExperimentalPermissionsApi::class)
+@SuppressLint("ClickableViewAccessibility", "MissingPermission")
 @Composable
 fun MapView(
     modifier: Modifier = Modifier,
     onLoad: ((map: MapView) -> Unit)? = null,
     onRequestStopInfo: (stopId: String) -> Unit,
     stopState: StateFlow<StopPredictionModel>,
-    stopsList: List<StopModel>
+    stopsList: List<StopModel>,
 ) {
     val mapViewState = rememberMapViewWithLifecycle()
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
     val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val fusedLocationClient = remember {
+        LocationServices.getFusedLocationProviderClient(
+            context
+        )
+    }
+    var userLocation: Location? = remember {null}
+    val permissionsState =
+        rememberMultiplePermissionsState(
+            permissions = listOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    if (permissionsState.allPermissionsGranted) {
+        LaunchedEffect(key1 = fusedLocationClient.lastLocation) {
+            if (permissionsState.allPermissionsGranted) {
+                fusedLocationClient.locationFlow(this).collect { location ->
+                    if (userLocation?.latitude != location?.latitude
+                        && userLocation?.longitude != location?.longitude
+                    ) {
+                        userLocation = location
+                    }
+                }
+            }
+        }
+    }
+
+
 
     DisposableEffect(key1 = lifecycle, key2 = stopsList) {
         val lifecycleObserver =
@@ -75,115 +117,131 @@ fun MapView(
             lifecycle.removeObserver(lifecycleObserver)
         }
     }
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(
+            factory = {
+                with(mapViewState) {
+                    // This fixes mapview overflowing parent layout
+                    clipToOutline = true
+                    controller.setCenter(GeoPoint(STARTING_LATITUDE, STARTING_LONGITUDE))
+                    controller.setZoom(START_ZOOM)
+                    setMultiTouchControls(true)
+                    zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+                }
+                val mapNorthCompassOverlay = object : CompassOverlay(context, mapViewState) {
+                    override fun draw(c: Canvas?, pProjection: Projection?) {
+                        drawCompass(c, -mapViewState.mapOrientation, pProjection?.screenRect)
+                    }
 
-    AndroidView(
-        factory = {
-            with(mapViewState) {
-                // This fixes mapview overflowing parent layout
-                clipToOutline = true
-                controller.setCenter(GeoPoint(STARTING_LATITUDE, STARTING_LONGITUDE))
-                controller.setZoom(START_ZOOM)
-                setMultiTouchControls(true)
-                zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
-            }
-            val mapNorthCompassOverlay = object : CompassOverlay(context, mapViewState) {
-                override fun draw(c: Canvas?, pProjection: Projection?) {
-                    drawCompass(c, -mapViewState.mapOrientation, pProjection?.screenRect)
+                    override fun onSingleTapConfirmed(e: MotionEvent?, mapView: MapView?): Boolean {
+                        if (e?.actionMasked == MotionEvent.ACTION_DOWN) {
+                            if (mapView == null) return false
+                            val mapViewPosition = IntArray(2)
+                            mapView.getLocationOnScreen(mapViewPosition)
+                            val frameLeft =
+                                (ceil((35.0f * mScale - mCompassRoseBitmap.width / 2) + mapViewPosition[0]).toInt())
+                            val frameTop =
+                                (ceil((35.0f * mScale - mCompassRoseBitmap.height / 2 + mapViewPosition[1])).toInt())
+                            val frameRight =
+                                (ceil((35.0f * mScale + mCompassRoseBitmap.width / 2 + mapViewPosition[0])).toInt())
+                            val frameBottom =
+                                (ceil((35.0f * mScale + mCompassRoseBitmap.height / 2 + mapViewPosition[1])).toInt())
+
+                            if (e.rawX > frameLeft && e.rawX < frameRight && e.rawY > frameTop && e.rawY < frameBottom) {
+                                mapView.controller.animateTo(
+                                    mapView.mapCenter, mapView.zoomLevelDouble,
+                                    1000L, 0f
+                                )
+                                return true
+                            }
+                        }
+                        return false
+                    }
                 }
 
-                override fun onSingleTapConfirmed(e: MotionEvent?, mapView: MapView?): Boolean {
-                    if (e?.actionMasked == MotionEvent.ACTION_DOWN) {
-                        if (mapView == null) return false
-                        val mapViewPosition = IntArray(2)
-                        mapView.getLocationOnScreen(mapViewPosition)
-                        val frameLeft =
-                            (ceil((35.0f * mScale - mCompassRoseBitmap.width / 2) + mapViewPosition[0]).toInt())
-                        val frameTop =
-                            (ceil((35.0f * mScale - mCompassRoseBitmap.height / 2 + mapViewPosition[1])).toInt())
-                        val frameRight =
-                            (ceil((35.0f * mScale + mCompassRoseBitmap.width / 2 + mapViewPosition[0])).toInt())
-                        val frameBottom =
-                            (ceil((35.0f * mScale + mCompassRoseBitmap.height / 2 + mapViewPosition[1])).toInt())
+                val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), mapViewState)
 
-                        if (e.rawX > frameLeft && e.rawX < frameRight && e.rawY > frameTop && e.rawY < frameBottom) {
-                            mapView.controller.animateTo(
-                                mapView.mapCenter, mapView.zoomLevelDouble,
-                                1000L, 0f
-                            )
-                            return true
+                // Override to improve map rotation smoothness - decreased deltaTime
+                var timeLastSet = 0L
+                val deltaTime = 10L
+                var currentAngle = 0f
+                val rotationOverlay = object : RotationGestureOverlay(mapViewState) {
+                    override fun onRotate(deltaAngle: Float) {
+                        currentAngle += deltaAngle
+                        if (System.currentTimeMillis() - deltaTime > timeLastSet) {
+                            timeLastSet = System.currentTimeMillis()
+                            mapViewState.mapOrientation = mapViewState.mapOrientation + currentAngle
                         }
                     }
-                    return false
                 }
-            }
 
-            val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), mapViewState)
-
-            // Override to improve map rotation smoothness - decreased deltaTime
-            var timeLastSet = 0L
-            val deltaTime = 10L
-            var currentAngle = 0f
-            val rotationOverlay = object : RotationGestureOverlay(mapViewState) {
-                override fun onRotate(deltaAngle: Float) {
-                    currentAngle += deltaAngle
-                    if (System.currentTimeMillis() - deltaTime > timeLastSet) {
-                        timeLastSet = System.currentTimeMillis()
-                        mapViewState.mapOrientation = mapViewState.mapOrientation + currentAngle
+                val mapEventsOverlay = MapEventsOverlay(object : MapEventsReceiver {
+                    override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
+                        InfoWindow.closeAllInfoWindowsOn(mapViewState)
+                        return true
                     }
+
+                    override fun longPressHelper(p: GeoPoint?): Boolean {
+                        return false
+                    }
+                })
+
+                val overlays =
+                    listOf(
+                        rotationOverlay,
+                        RadiusMarkerClusterer(context),
+                        locationOverlay,
+                        mapNorthCompassOverlay
+                    )
+                mapViewState.overlays.addAll(overlays)
+
+
+
+                // Events Overlays needs to be first to listen to events
+                mapViewState.overlays.add(0, mapEventsOverlay)
+
+
+
+                mapViewState
+            },
+            modifier = modifier
+        ) { mapView ->
+
+            mapViewState.setOnTouchListener { view, motionEvent ->
+                if (motionEvent.actionMasked == MotionEvent.ACTION_UP) {
+                    val stops = filteredMarkersOverlay(
+                        context,
+                        stopsList,
+                        (view as MapView),
+                        onRequestStopInfo = { stopId -> onRequestStopInfo(stopId) },
+                        stopState,
+                        coroutineScope,
+                        boundingBox = view.boundingBox
+                    )
+                    view.overlays[2] = stops
+                    view.invalidate()
                 }
+                view.performClick()
             }
 
-            val mapEventsOverlay = MapEventsOverlay(object : MapEventsReceiver {
-                override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
-                    InfoWindow.closeAllInfoWindowsOn(mapViewState)
-                    return true
-                }
-
-                override fun longPressHelper(p: GeoPoint?): Boolean {
-                    return false
-                }
-            })
-
-            val overlays =
-                listOf(
-                    rotationOverlay,
-                    RadiusMarkerClusterer(context),
-                    locationOverlay,
-                    mapNorthCompassOverlay
-                )
-            mapViewState.overlays.addAll(overlays)
-
-
-
-            // Events Overlays needs to be first to listen to events
-            mapViewState.overlays.add(0, mapEventsOverlay)
-
-
-
-            mapViewState
-        },
-        modifier = modifier
-    ) { mapView ->
-
-        mapViewState.setOnTouchListener { view, motionEvent ->
-            if (motionEvent.actionMasked == MotionEvent.ACTION_UP) {
-                val stops = filteredMarkersOverlay(
-                    context,
-                    stopsList,
-                    (view as MapView),
-                    onRequestStopInfo = { stopId -> onRequestStopInfo(stopId) },
-                    stopState,
-                    coroutineScope,
-                    boundingBox = view.boundingBox
-                )
-                view.overlays[2] = stops
-                view.invalidate()
-            }
-            view.performClick()
+            onLoad?.invoke(mapView)
         }
 
-        onLoad?.invoke(mapView)
+
+        Button(modifier = Modifier
+            .padding(20.dp)
+            .align(Alignment.BottomEnd),onClick = {
+            val geoPoint = userLocation?.let { GeoPoint(it) }
+            geoPoint?.let {
+                mapViewState.controller.animateTo(it, mapViewState.zoomLevelDouble,
+            1000L, 0f) } }) {
+            Text(text = "Center Map")
+        }
     }
+
+
+
+
 }
 
 fun isStopWithinBoundBox(
