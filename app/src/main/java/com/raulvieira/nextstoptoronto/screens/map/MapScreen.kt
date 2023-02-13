626bd6f49728797c7f45052dfac70ba40617a4ba
Raul Vieira <raulguil@gmail.com>
2023-02-13 09:48:34 -0500

    Add no internet bar to Map screen

--- app/src/main/java/com/raulvieira/nextstoptoronto/screens/map/MapScreen.kt
+++ app/src/main/java/com/raulvieira/nextstoptoronto/screens/map/MapScreen.kt
@@ -1,11 +1,20 @@
 package com.raulvieira.nextstoptoronto.screens.map
 
 import android.Manifest
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
@@ -13,6 +22,9 @@ import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.raulvieira.nextstoptoronto.components.InternetStatusBar
+import isInternetOn
+import kotlinx.coroutines.delay
 
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -28,6 +40,9 @@ fun MapScreen(
             )
         )
     val lifecycleOwner = LocalLifecycleOwner.current
+    val scope = rememberCoroutineScope()
+    val isInternetOn by isInternetOn(LocalContext.current, scope).collectAsStateWithLifecycle()
+    var internetStatusBarVisible by remember { mutableStateOf(false) }
     val stopsList by viewModel.stopList.collectAsStateWithLifecycle()
 
     DisposableEffect(key1 = lifecycleOwner) {
@@ -42,15 +57,34 @@ fun MapScreen(
         }
     }
 
+    LaunchedEffect(isInternetOn) {
+        internetStatusBarVisible = if (!isInternetOn) {
+            true
+        } else {
+            delay(2000)
+            false
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        MapView(
-            modifier = Modifier.fillMaxSize(),
-            onRequestStopInfo = { stopId -> viewModel.setStopIdValue(stopId) },
-            stopState = viewModel.stopState,
-            stopsList = stopsList,
-            onCloseStopInfo = { viewModel.clearStopIdFlow() }
-        )
+        Column {
+            AnimatedVisibility(
+                visible = internetStatusBarVisible,
+                enter = expandVertically(),
+                exit = shrinkVertically()
+            ) {
+                InternetStatusBar(isConnected = isInternetOn)
+            }
+            MapView(
+                modifier = Modifier.fillMaxSize(),
+                onRequestStopInfo = { stopId -> viewModel.setStopIdValue(stopId) },
+                stopState = viewModel.stopState,
+                stopsList = stopsList,
+                onCloseStopInfo = { viewModel.clearStopIdFlow() }
+            )
+        }
+
     }
 }
